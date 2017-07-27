4adfc8584cb3381868428d3920a49c411dddbd2b
JHades <jhades.dev@gmail.com>
2017-07-27 13:00:31 +0200

    ng-template post

--- sample-app/src/app/app.component.ts
+++ sample-app/src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  template: `      
+
+      <ng-template>
+
+          <button class="tab-button" (click)="login()">{{loginText}}</button>
+
+          <button class="tab-button" (click)="signUp()">{{signUpText}}</button>
+
+      </ng-template>
+
+
+
+      <div class="lessons-list" *ngIf="lessons  else loading">
+          <div class="lessons-list">
+              {{lessons | json}}
+          </div>
+      </div>
+
+      
+      <ng-template [ngIf]="lessons" [ngIfElse]="loading">
+          <div class="lessons-list">
+            {{lessonsList | json}}
+          </div>
+      </ng-template>
+      
+      
+      <ng-template #loading>
+          <div>Loading...</div>
+      </ng-template>
+
+
+  `})
 export class AppComponent {
-  title = 'app';
+
+    loginText = 'Login';
+    signUpText = 'Sign Up';
+
+    lessons = ['Lesson 1', 'Lessons 2'];
+
+
+
+    login() {
+        console.log('Login');
+    }
+
+    signUp() {
+        console.log('Sign Up');
+    }
+
 }
--- sample-app/src/app/app.module.ts
+++ sample-app/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import 'rxjs/add/observable/of';
+
+
 @NgModule({
   declarations: [
     AppComponent
